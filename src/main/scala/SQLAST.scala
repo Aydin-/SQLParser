760f8e8724c3372432aef3a79ddbb052c95bd154
Thomas MacGahan <tmacgahan@Thomass-iMac.local>
2017-03-19 17:14:33 -0400

    updated gitignore, have a first try, not-compiling language

---
+++ src/main/scala/SQLAST.scala
@@ -0,0 +1,6 @@
+package sqlparser
+
+object SQLAST {
+	trait ASTNode
+	case class A( property: String ) extends ASTNode
+}
--- src/main/scala/SQLParseRules.scala
+++ src/main/scala/SQLParseRules.scala
@@ -5,5 +5,9 @@ import scala.util.parsing.combinator.syntactical._
 import scala.collection.mutable.ListBuffer
 import scala.annotation.tailrec
 
+import SQLAST._
+
 object SQLParseRules extends StandardTokenParsers with PackratParsers with Parsers {
+	lexical.reserved += ()
+	lazy val dummyRule: A = "a" ~ ident ^^ { case "a" ~ vv => A(vv) }
 }
