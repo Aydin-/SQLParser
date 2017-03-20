c2785224b761ef596e2c8e542f41d1b573df1524
Thomas MacGahan <tmacgahan@Thomass-iMac.local>
2017-03-19 20:22:43 -0400

    hacked a bit more on getting the bnf implemented.  moved keywords to a trait.

--- src/main/scala/SQLParseRules.scala
+++ src/main/scala/SQLParseRules.scala
@@ -6,29 +6,32 @@ import scala.util.parsing.combinator._
 import scala.util.parsing.combinator.syntactical._
 
 object SQLParseRules extends StandardTokenParsers with PackratParsers with Parsers {
-  lexical.reserved += ( "a", "b" )
-  lazy val ARule: Parser[A] = "a" ~ BRule ^^ { case "a" ~ vv => A(vv) }
-  lazy val BRule: Parser[B] = "b" ~ ident ^^ { case "b" ~ vv => B(vv) }
-
-  lexical.reserved += ( "A", "ABS", "ABSOLUTE", "ACTION", "ADA", "ADMIN", "AFTER", "ALWAYS", "ASC", "ASSERTION", "ASSIGNMENT", "ATTRIBUTE", "ATTRIBUTES", "AVG", "BEFORE", "BERNOULLI", "BREADTH", "C", "CARDINALITY", "CASCADE", "CATALOG", "CATALOG_NAME", "CEIL", "CEILING", "CHAIN", "CHARACTERISTICS", "CHARACTERS", "CHARACTER_LENGTH", "CHARACTER_SET_CATALOG", "CHARACTER_SET_NAME", "CHARACTER_SET_SCHEMA", "CHAR_LENGTH", "CHECKED", "CLASS_ORIGIN", "COALESCE", "COBOL", "CODE_UNITS", "COLLATION", "COLLATION_CATALOG", "COLLATION_NAME", "COLLATION_SCHEMA", "COLLECT", "COLUMN_NAME", "COMMAND_FUNCTION", "COMMAND_FUNCTION_CODE", "COMMITTED", "CONDITION", "CONDITION_NUMBER", "CONNECTION_NAME", "CONSTRAINTS", "CONSTRAINT_CATALOG", "CONSTRAINT_NAME", "CONSTRAINT_SCHEMA", "CONSTRUCTORS", "CONTAINS", "CONVERT", "CORR", "COUNT", "COVAR_POP", "COVAR_SAMP", "CUME_DIST", "CURRENT_COLLATION", "CURSOR_NAME", "DATA", "DATETIME_INTERVAL_CODE", "DATETIME_INTERVAL_PRECISION", "DEFAULTS", "DEFERRABLE", "DEFERRED", "DEFINED", "DEFINER", "DEGREE", "DENSE_RANK", "DEPTH", "DERIVED", "DESC", "DESCRIPTOR", "DIAGNOSTICS", "DISPATCH", "DOMAIN", "DYNAMIC_FUNCTION", "DYNAMIC_FUNCTION_CODE", "EQUALS", "EVERY", "EXCEPTION", "EXCLUDE", "EXCLUDING", "EXP", "EXTRACT", "FINAL", "FIRST", "FLOOR", "FOLLOWING", "FORTRAN", "FOUND", "FUSION", "G", "GENERAL", "GO", "GOTO", "GRANTED", "HIERARCHY", "IMPLEMENTATION", "INCLUDING", "INCREMENT", "INITIALLY", "INSTANCE", "INSTANTIABLE", "INTERSECTION", "INVOKER", "ISOLATION", "K", "KEY", "KEY_MEMBER", "KEY_TYPE", "LAST", "LENGTH", "LEVEL", "LN", "LOCATOR", "LOWER", "M", "MAP", "MATCHED", "MAX", "MAXVALUE", "MESSAGE_LENGTH", "MESSAGE_OCTET_LENGTH", "MESSAGE_TEXT", "MIN", "MINVALUE", "MOD", "MORE", "MUMPS", "NAME", "NAMES", "NESTING", "NEXT", "NORMALIZE", "NORMALIZED", "NULLABLE", "NULLIF", "NULLS", "NUMBER", "OBJECT", "OCTETS", "OCTET_LENGTH", "OPTION", "OPTIONS", "ORDERING", "ORDINALITY", "OTHERS", "OVERLAY", "OVERRIDING", "PAD", "PARAMETER_MODE", "PARAMETER_NAME", "PARAMETER_ORDINAL_POSITION", "PARAMETER_SPECIFIC_CATALOG", "PARAMETER_SPECIFIC_NAME", "PARAMETER_SPECIFIC_SCHEMA", "PARTIAL", "PASCAL", "PATH", "PERCENTILE_CONT", "PERCENTILE_DISC", "PERCENT_RANK", "PLACING", "PLI", "POSITION", "POWER", "PRECEDING", "PRESERVE", "PRIOR", "PRIVILEGES", "PUBLIC", "RANK", "READ", "RELATIVE", "REPEATABLE", "RESTART", "RETURNED_CARDINALITY", "RETURNED_LENGTH", "RETURNED_OCTET_LENGTH", "RETURNED_SQLSTATE", "ROLE", "ROUTINE", "ROUTINE_CATALOG", "ROUTINE_NAME", "ROUTINE_SCHEMA", "ROW_COUNT", "ROW_NUMBER", "SCALE", "SCHEMA", "SCHEMA_NAME", "SCOPE_CATALOG", "SCOPE_NAME", "SCOPE_SCHEMA", "SECTION", "SECURITY", "SELF", "SEQUENCE", "SERIALIZABLE", "SERVER_NAME", "SESSION", "SETS", "SIMPLE", "SIZE", "SOURCE", "SPACE", "SPECIFIC_NAME", "SQRT", "STATE", "STATEMENT", "STDDEV_POP", "STDDEV_SAMP", "STRUCTURE", "STYLE", "SUBCLASS_ORIGIN", "SUBSTRING", "SUM", "TABLESAMPLE", "TABLE_NAME", "TEMPORARY", "TIES", "TOP_LEVEL_COUNT", "TRANSACTION", "TRANSACTIONS_COMMITTED", "TRANSACTIONS_ROLLED_BACK", "TRANSACTION_ACTIVE", "TRANSFORM", "TRANSFORMS", "TRANSLATE", "TRIGGER_CATALOG", "TRIGGER_NAME", "TRIGGER_SCHEMA", "TRIM", "TYPE", "UNBOUNDED", "UNCOMMITTED", "UNDER", "UNNAMED", "USAGE", "USER_DEFINED_TYPE_CATALOG", "USER_DEFINED_TYPE_CODE", "USER_DEFINED_TYPE_NAME", "USER_DEFINED_TYPE_SCHEMA", "VIEW", "WORK", "WRITE", "ZONE" )
-
-  lexical.reserved += ( "ADD", "ALL", "ALLOCATE", "ALTER", "AND", "ANY", "ARE", "ARRAY", "AS", "ASENSITIVE", "ASYMMETRIC", "AT", "ATOMIC", "AUTHORIZATION", "BEGIN", "BETWEEN", "BIGINT", "BINARY", "BLOB", "BOOLEAN", "BOTH", "BY", "CALL", "CALLED", "CASCADED", "CASE", "CAST", "CHAR", "CHARACTER", "CHECK", "CLOB", "CLOSE", "COLLATE", "COLUMN", "COMMIT", "CONNECT", "CONSTRAINT", "CONTINUE", "CORRESPONDING", "CREATE", "CROSS", "CUBE", "CURRENT", "CURRENT_DATE", "CURRENT_DEFAULT_TRANSFORM_GROUP", "CURRENT_PATH", "CURRENT_ROLE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_TRANSFORM_GROUP_FOR_TYPE", "CURRENT_USER", "CURSOR", "CYCLE", "DATE", "DAY", "DEALLOCATE", "DEC", "DECIMAL", "DECLARE", "DEFAULT", "DELETE", "DEREF", "DESCRIBE", "DETERMINISTIC", "DISCONNECT", "DISTINCT", "DOUBLE", "DROP", "DYNAMIC", "EACH", "ELEMENT", "ELSE", "END", "END-EXEC", "ESCAPE", "EXCEPT", "EXEC", "EXECUTE", "EXISTS", "EXTERNAL", "FALSE", "FETCH", "FILTER", "FLOAT", "FOR", "FOREIGN", "FREE", "FROM", "FULL", "FUNCTION", "GET", "GLOBAL", "GRANT", "GROUP", "GROUPING", "HAVING", "HOLD", "HOUR", "IDENTITY", "IMMEDIATE", "IN", "INDICATOR", "INNER", "INOUT", "INPUT", "INSENSITIVE", "INSERT", "INT", "INTEGER", "INTERSECT", "INTERVAL", "INTO", "IS", "ISOLATION", "JOIN", "LANGUAGE", "LARGE", "LATERAL", "LEADING", "LEFT", "LIKE", "LOCAL", "LOCALTIME", "LOCALTIMESTAMP", "MATCH", "MEMBER", "MERGE", "METHOD", "MINUTE", "MODIFIES", "MODULE", "MONTH", "MULTISET", "NATIONAL", "NATURAL", "NCHAR", "NCLOB", "NEW", "NO", "NONE", "NOT", "NULL", "NUMERIC", "OF", "OLD", "ON", "ONLY", "OPEN", "OR", "ORDER", "OUT", "OUTER", "OUTPUT", "OVER", "OVERLAPS", "PARAMETER", "PARTITION", "PRECISION", "PREPARE", "PRIMARY", "PROCEDURE", "RANGE", "READS", "REAL", "RECURSIVE", "REF", "REFERENCES", "REFERENCING", "REGR_AVGX", "REGR_AVGY", "REGR_COUNT", "REGR_INTERCEPT", "REGR_R2", "REGR_SLOPE", "REGR_SXX", "REGR_SXY", "REGR_SYY", "RELEASE", "RESULT", "RETURN", "RETURNS", "REVOKE", "RIGHT", "ROLLBACK", "ROLLUP", "ROW", "ROWS", "SAVEPOINT", "SCROLL", "SEARCH", "SECOND", "SELECT", "SENSITIVE", "SESSION_USER", "SET", "SIMILAR", "SMALLINT", "SOME", "SPECIFIC", "SPECIFICTYPE", "SQL", "SQLEXCEPTION", "SQLSTATE", "SQLWARNING", "START", "STATIC", "SUBMULTISET", "SYMMETRIC", "SYSTEM", "SYSTEM_USER", "TABLE", "THEN", "TIME", "TIMESTAMP", "TIMEZONE_HOUR", "TIMEZONE_MINUTE", "TO", "TRAILING", "TRANSLATION", "TREAT", "TRIGGER", "TRUE", "UESCAPE", "UNION", "UNIQUE", "UNKNOWN", "UNNEST", "UPDATE", "UPPER", "USER", "USING", "VALUE", "VALUES", "VAR_POP", "VAR_SAMP", "VARCHAR", "VARYING", "WHEN", "WHENEVER", "WHERE", "WIDTH_BUCKET", "WINDOW", "WITH", "WITHIN", "WITHOUT", "YEAR" )
-
-  lazy val SQLObjectIdentifier: Parser[SQLObjectIdentifierRule] = <SQL provenance> <SQL variant>
-  lazy val SQLProvenance: Parser[SQLProvenanceRule] = <arc1> <arc2> <arc3>
-  lazy val SQLVariant: Parser[SQLVariantRule] = <SQL edition> <SQL conformance>
-  lazy val SQLEdition: Parser[SQLEditionRule] = <1987> | <1989> | <1992> | <1999> | <200n>
-<1987> ::= 0 | edition1987 <left paren> 0 <right paren>
-<1989> ::= <1989 base> <1989 package>
-  lazy val SQLConformance: Parser[SQLConformanceRule] = <level> <bindings> <parts> <packages>
-<level> ::= <low> | <intermediate> | <high>
-<low> ::= 0 | Low <left paren> 0 <right paren>
-<intermediate> ::= 1 | Intermediate <left paren> 1 <right paren>
-<high> ::= 2 | High <left paren> 2 <right paren>
-<arc1> ::= iso | 1 | iso <left paren> 1 <right paren>
-<arc2> ::= standard | 0 | standard <left paren> 0 <right paren>
-<arc3> ::= 9075
-
-  lazy val leftParen: Parser[LeftParenRule] = (
-  lazy val rightParen: Parser[RightParenRule] = )
+	lazy val sqlObjectIdentifier: Parser[SQLObjectIdentifierRule] = sqlProvenance ~ sqlVariant
+	lazy val sqlProvenance: Parser[SQLProvenanceRule] = arc1 ~ arc2 ~ arc3
+	lazy val sqlVariant: Parser[SQLVariantRule] = sqlEdition ~ sqlConformance
+	lazy val sqlEdition: Parser[SQLEditionRule] = y1987 | y1989 | y1992 | y1999 | y200n
+	lazy val y1987: Parser[Y1987Rule] = "0" | ("edition1987" ~ leftParen ~ "0" ~ rightParen)
+	lazy val y1989: Parser[Y1989Rule] = y1989base ~ y1989package
+	lazy val sqlConformance: Parser[SQLConformanceRule] = level ~ bindings ~ parts ~ packages
+	lazy val level: Parser[LevelRule] = low | intermediate | high
+	lazy val low: Parser[LowRule] = "0" | "Low" ~ leftParen ~ "0" ~ rightParen
+	lazy val intermediate: Parser[IntermediateRule] = "1" | "Intermediate" ~ leftParen ~ "1" ~ rightParen
+	lazy val high: Parser[HighRule] = "2" | "High" ~ leftParen ~ "2" ~ rightParen
+	lazy val arc1: Parser[Arc1Rule] = "iso" | "1" | "iso" leftParen 1 rightParen
+	lazy val arc2: Parser[Arc2Rule] = "standard" | "0" | ("standard" ~ leftParen ~ "0" ~ rightParen)
+	lazy val arc3: Parser[Arc3Rule] = "9075"
+	lazy val leftParen: Parser[LeftParenRule] = "("
+	lazy val rightParen: Parser[RightParenRule] = ")"
+	lazy val sqlTerminalCharacter: Parser[SqlTerminalCharacterRule] = sqlLanguageCharacter
+	lazy val sqlLanguageCharacter: Parser[SqlLanguageCharacterRule] = simpleLatinLetter | digit | sqlSpecialCharacter
+	lazy val simpleLatinLetter: Parser[SimpleLatinLetterRule] = simpleLatinUpperCaseLetter | simpleLatinLowerCaseLetter
+	lazy val simpleLatinUpperCaseLetter: Parser[SimpleLatinUpperCaseLetterRule] = "A" | "B" | "C" | "D" | "E" | "F" | "G" |
+	  	"H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
+	lazy val simpleLatinLowerCaseLetter: Parser[SimpleLatinLowerCaseLetterRule] = "a" | "b" | "c" | "d" | "e" | "f" | "g" |
+		"h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+	lazy val digit: Parser[DigitRule] = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+	lazy val sqlSpecialCharacter: Parser[SqlSpecialCharacterRule] = space | doubleQuote | percent | ampersand | quote |
+ 	 	leftParen | rightParen | asterisk | plusSign | comma | minusSign | period | solidus | colon | semicolon |
+		lessThanOperator | equalsOperator | greaterThanOperator | questionMark | leftBracket | rightBracket | circumflex |
+		underscore | verticalBar | leftBrace | rightBrace
 }
